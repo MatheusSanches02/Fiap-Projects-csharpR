ead8c84d2e52d0f7cd20834dfd5c4e20ca01b0b5
MatheusSanches02 <rm87426@fiap.com.br>
2022-08-14 22:08:27 -0300

    introduction csharp console

---
+++ Fiap.Exercicio.UI/Models/ContaCorrente.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiap.Exercicio.UI.Models
+{
+    internal class ContaCorrente : Conta
+    {
+        public TipoConta Tipo { get; set; }
+        public decimal Limite { get; set; }
+
+        public ContaCorrente(int agencia, int numero, IList<Cliente> clientes, TipoConta tipo) : base(agencia, numero, clientes)
+        {
+            Tipo = tipo;
+        }
+
+        public override void Depositar(decimal valor)
+        {
+            Saldo = Saldo + valor;
+        }
+
+        public override void Retirar(decimal valor)
+        {
+            Saldo = Saldo - valor;
+        }
+    }
+
+    public enum TipoConta
+    {
+        Comum, Especial, Premium
+    }
+}
