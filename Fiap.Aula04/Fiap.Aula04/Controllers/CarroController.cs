5fad1b634789b25d08a65294ef8184050cf62428
MatheusSanches02 <rm87426@fiap.com.br>
2022-09-01 22:53:36 -0300

    tag helpers

---
+++ Fiap.Aula04/Fiap.Aula04/Controllers/CarroController.cs
@@ -0,0 +1,31 @@
+﻿using Fiap.Aula04.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fiap.Aula04.Controllers
+{
+    public class CarroController : Controller
+    {
+
+        private static List<Carro> _carroList = new List<Carro>();
+
+        public IActionResult Index()
+        {
+            ViewBag.lista = _carroList;
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Carro carro)
+        {
+            _carroList.Add(carro);
+            ViewBag.mensagemSucesso = "Carro cadastrado!";
+            return View();
+        }
+    }
+}
---
+++ Fiap.Aula04/Fiap.Aula04/Models/Carro.cs
@@ -0,0 +1,10 @@
+﻿namespace Fiap.Aula04.Models
+{
+    public class Carro
+    {
+        public string? Modelo { get; set; }
+        public int Ano { get; set; }
+        public bool Automatico { get; set; }
+        public decimal Valor { get; set; }
+    }
+}
