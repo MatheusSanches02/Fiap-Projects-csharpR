5bd2b743ccdd8dff30502b81ec849672a140cea9
MatheusSanches02 <rm87426@fiap.com.br>
2022-09-14 20:52:32 -0300

    redirecttoaction + editar

--- Fiap.Aula04/Fiap.Aula04/Controllers/CarroController.cs
+++ Fiap.Aula04/Fiap.Aula04/Controllers/CarroController.cs
@@ -1,31 +1,46 @@
 ﻿using Fiap.Aula04.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Fiap.Aula04.Controllers
 {
     public class CarroController : Controller
     {
-
+        //Lista estática para armazenar carros
         private static List<Carro> _carroList = new List<Carro>();
+        //Contador estático para definir o código do veiculo
+        private static int _id = 0;
 
         public IActionResult Index()
         {
-            ViewBag.lista = _carroList;
-            return View();
+            //ViewBag.lista = _carroList;
+            return View(_carroList);
         }
 
         [HttpGet]
         public IActionResult Cadastrar()
         {
+            var fabricantes = new List<string>(new string[] {"Nissan", "Honda", "Fiat", "Toyota", "Renault", "Volkswagen"});
+
+            ViewBag.Fabricantes = new SelectList(fabricantes);
             return View();
         }
 
         [HttpPost]
         public IActionResult Cadastrar(Carro carro)
         {
+            carro.Codigo = ++_id;
             _carroList.Add(carro);
-            ViewBag.mensagemSucesso = "Carro cadastrado!";
-            return View();
+            TempData["mensagemSucesso"] = "Carro cadastrado!";
+            return RedirectToAction("Cadastrar");
+        }
+
+        [HttpGet]
+        public IActionResult Editar(int id)
+        {
+            var carro = _carroList.Find(match => match.Codigo == id);
+
+            return View(carro);
         }
     }
 }
--- Fiap.Aula04/Fiap.Aula04/Models/Carro.cs
+++ Fiap.Aula04/Fiap.Aula04/Models/Carro.cs
@@ -1,10 +1,23 @@
-﻿namespace Fiap.Aula04.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Fiap.Aula04.Models
 {
     public class Carro
     {
+        public int Codigo { get; set; }
         public string? Modelo { get; set; }
+        public string? Fabricante { get; set; }
         public int Ano { get; set; }
+
+        [Display(Name = "Automático")]
         public bool Automatico { get; set; }
         public decimal Valor { get; set; }
+        [Display(Name = "Combustível")]
+        public Combustivel? Combustivel { get; set; }
+    }
+
+    public enum Combustivel
+    {
+        Etanol, Gasolina, Flex, Eletrico
     }
 }
