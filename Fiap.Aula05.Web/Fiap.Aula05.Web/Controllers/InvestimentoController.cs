e7e2a790147e15179e77829f9d0c8f92274c5620
MatheusSanches02 <rm87426@fiap.com.br>
2022-09-28 20:57:36 -0300

    Introdução ao entity framework

---
+++ Fiap.Aula05.Web/Fiap.Aula05.Web/Controllers/InvestimentoController.cs
@@ -0,0 +1,31 @@
+﻿using Fiap.Aula05.Web.Models;
+using Fiap.Aula05.Web.Models.Persistencia;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fiap.Aula05.Web.Controllers
+{
+    public class InvestimentoController : Controller
+    {
+        private DimDimContext _context;
+        public InvestimentoController(DimDimContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Investimento investimento)
+        {
+            return View();
+        }
+    }
+}
---
+++ Fiap.Aula05.Web/Fiap.Aula05.Web/Models/Persistencia/DimDimContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Fiap.Aula05.Web.Models.Persistencia
+{
+    public class DimDimContext : DbContext
+    {
+        public DbSet<Investimento> Investimentos { get; set; } //set conjunto que nao possui dados indexados e nao repetem os elementos
+        public DimDimContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
